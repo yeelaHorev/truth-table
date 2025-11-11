d1d3c1f2a24ba3addfb8c88109b4792919e98517
Yeela Horev <yeelahorev@hilma.tech>
2025-11-11 11:37:48 +0200

    what?

--- start-app.bat
+++ start-app.bat
@@ -1,6 +1,11 @@
 @echo off
+
+SET iconPath=%~dp0src\assets\namma.ico
 :: Change to the directory of the batch file
 cd /d %~dp0
+
+:: Create the shortcut using PowerShell
+powershell -Command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%desktopPath%'); $s.TargetPath='%appPath%'; $s.IconLocation='%iconPath%'; $s.Save()"
 
 :: Start Node server in background
 start "" cmd /c "node server.js"
